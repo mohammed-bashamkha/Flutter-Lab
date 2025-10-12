ab48d1ae106775cfc3a9ecb1eb1c9aa339794dcb
mohammed_bashamkha <mb877402@gmail.com>
2025-10-12 17:13:40 +0300

    Implement GetApi page to fetch and display user information from API

--- future/lib/api.dart
+++ future/lib/api.dart
@@ -1,10 +1,72 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:future/user_class.dart';
+import 'package:http/http.dart' as http;
 
 class GetApi extends StatelessWidget {
   const GetApi({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Get User Info From API',
+          style: TextStyle(
+            backgroundColor: Colors.blueGrey,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+      body: FutureBuilder<User>(
+        future: fetchUser(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('أطخ ثدح: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final user = snapshot.data!;
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: ListTile(
+                  leading: CircleAvatar(child: Text(user.name[0])),
+                  title: Text(
+                    user.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'UserName: ${user.username}\n Email: ${user.email}',
+                  ),
+                ),
+              ),
+            );
+          } else {
+            return const Center(child: Text('No Data Found'));
+          }
+        },
+      ),
+    );
+  }
+}
+
+Future<User> fetchUser() async {
+  final response = await http.get(
+    Uri.parse('https://jsonplaceholder.typicode.com/users/1'),
+  );
+
+  if (response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Faild To Load User Data');
   }
 }
